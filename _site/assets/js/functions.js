f552d279903a64092e50eb37f65b2fe35be2eaea
Charles Patton <cdpatton2009@gmail.com>
2019-09-27 13:24:54 -0700

    Changed default layout to include all js in every file header. Moved DA TOC into a yaml file. Decided that the contents.html should just list sutras as they are translated (like Access to Insight).

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,3 +1,12 @@
+function toggleSubmenu(id) {
+  var submenu = document.getElementById(id);
+  if (submenu.style.display === "block") {
+    submenu.style.display = "none";
+  } else {
+    submenu.style.display = "block";
+  }
+}
+
 function toggleChinese() {
   var elements = document.querySelectorAll('[class="ch"]');
 
--- assets/js/sidebar.js
+++
@@ -1,8 +0,0 @@
-function toggleSubmenu(id) {
-  var submenu = document.getElementById(id);
-  if (submenu.style.display === "block") {
-    submenu.style.display = "none";
-  } else {
-    submenu.style.display = "block";
-  }
-}
