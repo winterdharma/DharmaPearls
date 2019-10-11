4e25bf96c7c5543a34686d0335489ea9af4093d1
Charles Patton <cdpatton2009@gmail.com>
2019-10-10 20:31:21 -0700

    Decided to split the Glossary off into another project, perhaps a personal translation tool. Removed all glossary related files. Also decided for the time being to nix a bilinguial presentation and the sidebars. Switching to a centered single column format for simplicity sake. Header styles changed. Need to handle notes differently.

--- _site/04_ekottarika/EA_01.h
+++ _site/04_ekottarika/EA_01.h
@@ -33,12 +33,10 @@
 	<h1><a href="../index.html">Dharma Pearls</a></h1>
 	<h2>Translating Early Buddhism to Modern English</h2>
 	<nav>
-		<ul>
-			<li><a href="../whats-new.html">What's New</a></li>
-			<li><a href="../translations.html">Translations</a></li>
-			<li><a href="../resources.html">Resources</a></li>
-			<li><a href="../about.html">About</a></li>
-		</ul>
+			<a class="nav-item" href="../whats-new.html">What's New</a></li>
+			<a class="nav-item" href="../translations.html">Translations</a></li>
+			<a class="nav-item" href="../resources.html">Resources</a></li>
+			<a class="nav-item last" href="../about.html">About</a></li>
 	</nav>
 </header>
 
@@ -49,58 +47,9 @@
 
 
 <div class="translation">
-  <nav class="sidebar">
-	<div class="img"><img src="../assets/img/dharmapearls_icon_transparent.png"/></div>
-
-	<ul class="top">
-		<li><a href="/index.html">Introduction</a></li>
-		<li>Translations</li>
-			<ul class="submenu">
-				<li><img onclick="toggleSubmenu('dirgha');" class="inline-icon" src="../assets/img/more-icon.svg"/><a class="title" href="../01_dirgha/contents.html">The Long Discourses</a></li>
-				<ol id="dirgha" start="29" style="display: none;">
-					<li><a href="../01_dirgha/DA_29.html">Lohitya</a></li>
-				</ol>
-				<li><img onclick="toggleSubmenu('madhyama');" class="inline-icon" src="../assets/img/more-icon.svg"/><a class="title" href="../02_madhyama/contents.html">The Medium Discourses</a></li>
-				<ol id="madhyama" style="display: none;">
-					<li><a href="../02_madhyama/MA_001.html">Good Dharma</a></li>
-					<li><a href="../02_madhyama/MA_002.html">Pārijāta Tree</a></li>
-					<li><a href="../02_madhyama/MA_003.html">City Parable</a></li>
-					<li><a href="../02_madhyama/MA_004.html">Water Parable</a></li>
-					<li><a href="../02_madhyama/MA_005.html">Wood Pile Parable</a></li>
-					<li><a href="../02_madhyama/MA_006.html">Good Person's Departure</a></li>
-					<li><a href="../02_madhyama/MA_007.html">Worldly Merit</a></li>
-				</ol>
-				<!-- <li>The Related Discourses</li>
-				<!-- <li><a href="../04_ekottarika/contents.html">The Numerical Discourses</a></li>
-				<li>Other Minor Texts</li> -->
-			</ul>
-		<!-- <li>Index of Pali Equivalents</li> -->
-		<li><a href="../X2_glossary/index.html">Chinese-English Glossary</a></li>
-		<li><a href="../copyright.html">Copyright Policy</a></li>
-	</ul>
-	
-		<div class="img"><button id="toggleChinese" onclick="toggleChinese();">Show Chinese</button><br/>
-		<button id="toggleNotes" onclick="toggleNotes();">Hide Notes</button></div>
-	
-</nav>
-
   <article class="sutra">
-    <section class="ch" style="display: none;">
-  <h1>增壹阿含經</h1>
-</section>
-
-<section class="en" style="display: block;">
-  <h1>The Numerical Discourses</h1>
-</section>
-
-    <section class="ch" style="display: none;">
-  <h2 id="ch.0">(一) 序</h2>
-</section>
-
-<section class="en">
-  <h2 id="en.0">1. Introduction</h2>
-</section>
-
+      <h1 class="en">The Numerical Discourses</h1>
+      <h2 class="en" id="en.0">1. Introduction</h2>
 
     
     <section class="footer">
